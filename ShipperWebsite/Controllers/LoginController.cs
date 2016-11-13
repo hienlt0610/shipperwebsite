1b713215c9b328962eb6741bd3d06365799429c0
Hien Le <hienlt0610@gmail.com>
2016-11-14 00:13:44 +0700

    init website

---
+++ ShipperWebsite/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShipperWebsite.Controllers
+{
+    public class LoginController : BaseController
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ ShipperWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShipperWebsite.Startup))]
+namespace ShipperWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
