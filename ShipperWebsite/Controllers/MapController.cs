a503194c328ec845073414522bd1e031d663d082
Hien Le <hienlt0610@gmail.com>
2016-11-14 23:06:39 +0700

    them chuc nang realtime xem ban do nhan vien di chuyen

---
+++ ShipperWebsite/Controllers/MapController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShipperWebsite.Controllers
+{
+    public class MapController : BaseController
+    {
+        //
+        // GET: /Map/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
